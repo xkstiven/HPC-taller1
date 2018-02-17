7b0dad32d9c1a6f5456abd5eb90869d2bfea59e7
xkstiven <dsv-zapata666@hotmail.com>
2018-02-16 20:32:11 -0500

    vercion final
    se implementa la funcion random, memoria dinamica (malloc),
    almacenamiento en archivo .cvs, se cambia la vercion de prueba que solo
    usaba enteros ahorra corre con flotantes

--- multimatrices.c
+++ multimatrices.c
@@ -1,6 +1,8 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 int main(){
+	srand(time(NULL));
 	int af,ac,bf,bc,i,j,k;
 	
 	printf("numero de filas matriz A:");
@@ -17,20 +19,38 @@ int main(){
 		return 0;
 	}
 	
-	int A[af][ac], B[bf][bc], C[af][bc];
+	FILE *archivo;
+  	archivo = fopen("taller1-2.cvs","w");
+	
+	float **A;
+	A= (float **)malloc(af*sizeof(float *));
+	for(i=0;i<af;i++){
+		A[i]= (float *)malloc(ac*sizeof(float));
+	}
+	
+	float **B;
+	B= (float **)malloc(bf*sizeof(float *));
+	for(i=0;i<bf;i++){
+		B[i]= (float *)malloc(bc*sizeof(float));
+	}
+	
+	float **C;
+	C= (float **)malloc(af*sizeof(float *));
+	for(i=0;i<af;i++){
+		C[i]= (float *)malloc(bc*sizeof(float));
+	}
 	
 	for(i=0;i<af;i++){
 		for(j=0;j<ac;j++){
-			printf("escriba el valor de la matris 1 (%d,%d)-->",i+1,j+1);
-			scanf("%d",&A[i][j]);
+			float valor = 1+ rand() % (11-1);
+      		A[i][j]= (float)(valor / 11.0);
 		}
 	}
 	
-	printf("\n\n");
 	for(i=0;i<bf;i++){
 		for(j=0;j<bc;j++){
-			printf("escriba el valor de la matris 2 (%d,%d)-->",i+1,j+1);
-			scanf("%d",&B[i][j]);
+			float valor = 1+ rand() % (11-1);
+      		B[i][j]= (float)(valor / 3.0);
 		}
 	}
 	
@@ -43,33 +63,38 @@ int main(){
 		}
 	}
 	
-	printf("\n\n\t\t\t Matriz A");
-	printf("\n");
 	for(i=0;i<af;i++){
-		printf("\n\t\t");
+		printf("\n");
+		fprintf(archivo,"%s","\n");
 		for(j=0;j<ac;j++){
-			printf("  %d",A[i][j]);
+			printf("%f2.2   ",A[i][j]);
+			fprintf(archivo,"%f   ",A[i][j]);
 		}
 	}
 	
-	printf("\n\n\t\t\t Matriz B");
-	printf("\n");
+	printf("\n\n");
+	fprintf(archivo,"%s","\n\n");
 	for(i=0;i<bf;i++){
-		printf("\n\t\t");
+		printf("\n");
+		fprintf(archivo,"%s","\n");
 		for(j=0;j<bc;j++){
-			printf("  %d",B[i][j]);
+			printf("%f2.2   ",B[i][j]);
+			fprintf(archivo,"%f   ",B[i][j]);
 		}
 	}
 	
-	printf("\n\n\t\t\t Matriz resultado");
+	printf("\n\n");
 	printf("\n");
+	fprintf(archivo,"%s","\n\n\n");
 	for(i=0;i<af;i++){
-		printf("\n\t\t");
+		printf("\n");
+		fprintf(archivo,"%s","\n");
 		for(j=0;j<bc;j++){
-			printf("  %d",C[i][j]);
+			printf("%f2.2   ",C[i][j]);
+			fprintf(archivo,"%f   ",C[i][j]);
 		}
 	}
-	printf("\n");
+	printf("\n\n\n");
 	system("PAUSE");
 	
 }
